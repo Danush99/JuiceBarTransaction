b54599868e3e6970abdb6f018a470cf0efeeeaa3
Danusha <danush99online@gmail.com>
2022-08-05 13:19:06 +0530

    created fundamental funtions

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ import priceList
 price_list = priceList.prieList
 
 
+
 def change_availability(juice_name,availability):
     price_list[juice_name][1]=availability;
-    
+
+def add_item():
+    print("added an intem")
+
+def remove_item():
+    print("removed an item")
+
+def print_bill():
+    print("bill")
+
+def show_total_sales():
+    print("total sales")
+
+
 
 print("prieList",priceList.prieList)
 change_availability("Mango Juice",1)
